e6f611bc3a28ae286937ec9e263b027cfc108c64
snakeee <1679634762@qq.com>
2021-06-04 11:54:10 +0800

    完成二次均匀B样条曲线

--- CourseDesign/main.cpp
+++ CourseDesign/main.cpp
@@ -1,5 +1,6 @@
 #include <GLUT/GLUT.h>
 #include <iostream>
+#include <cmath>
 #include <cstdio>
 using namespace std;
 #define LEFT_EDGE   1
@@ -17,11 +18,34 @@ struct MyRect
 };
 
 MyRect rect;
-int x0, y0, x1, y1;
-int px0, py0, px1, py1;
+int x2, y2, x3, y3;
+int px2, py2, px3, py3;
 bool bDrawLine = true;
 int width = 640, height = 480;
 /*直线裁剪算法所用数据结构*/
+
+
+/*B样条曲线所用数据结构*/
+#define NUM_POINTS 4
+#define NUM_SEGMENTS NUM_POINTS - 2 // 阶数
+
+int cnt = NUM_POINTS; // 这里表示目前有多少个点
+
+struct Point2
+{
+    double x;
+    double y;
+
+    Point2() { ; }
+    Point2(int px, int py) { x = px; y = py; }
+    void SetPoint2(int px, int py) { x = px; y = py; }
+};
+
+/*全局变量*/
+Point2 vec[100];
+bool mouseLeftDown = false;//记录鼠标左右键事件触发
+/*B样条曲线所用数据结构*/
+
 
 void LineGL(int x0, int  y0, int x1, int y1)
 {
@@ -139,47 +163,81 @@ bool LBLineClip(MyRect rect, int &x0, int &y0, int &x1, int &y1) {
 }
 
 
-void myDisplay()
-{
-    glClear(GL_COLOR_BUFFER_BIT);
-    glColor3f(1.0f, 1.0f, 0.0f);
-    glRectf(rect.xmin, rect.ymin, rect.xmax, rect.ymax);
+/*绘制B样条曲线*/
+void Bspline(int n) {
+    float f1, f2, f3;
+    float deltaT = 1.0 / n;
+    float T;
 
-    if (bDrawLine)
-        LineGL(x0, y0, x1, y1);
+    glBegin(GL_LINE_STRIP);
+    // NUM_SEGMENTS = 点数 - 2（二阶）
+    for (int num = 0; num < cnt - 2; num++) {
+        for (int i = 0; i <= n; i++) {
+            T = i * deltaT;
+            f1 = (1-T) *(1-T) / 2;
+            f2 = (T + 1)*(1 - T) / 2 + (T) * (2 - T) / 2;
+            f3 = T * T / 2;
 
-    glFlush();
+            glVertex2f(f1*vec[num].x + f2*vec[num+1].x + f3*vec[num+2].x,
+                       f1*vec[num].y + f2*vec[num+1].y + f3*vec[num+2].y);
+        }
+    }
+    glEnd();
+}
+
+double distance(int x1, int y1, int x2, int y2) {
+    return sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
 }
 
 void init_line()
 {
-    glClearColor(0.0, 0.0, 0.0, 0.0);
+    glClearColor(1.0, 1.0, 1.0, 0.0);
     glShadeModel(GL_FLAT);
 
     rect.xmin = 100; //窗口的大小
     rect.xmax = 300;
     rect.ymin = 100;
     rect.ymax = 300;
-    x0 = 300; y0 = 50; x1 = 50; y1 = 450;
-    px0 = x0; py0 = y0; px1 = x1; py1 = y1;
-
-    glClear(GL_COLOR_BUFFER_BIT);
-    glColor3f(0.0f, 1.0f, 0.0f);
-    glRectf(rect.xmin, rect.ymin, rect.xmax, rect.ymax);
-
+    x2 = 300; y2 = 50; x3 = 50; y3 = 450;
+    px2 = x2; py2 = y2; px3 = x3; py3 = y3;
 }
 
-void restore() {
-    x0 = px0; x1 = px1; y0 = py0; y1 = py1;
+void init_Bspline() {
+    glClearColor(1.0, 1.0, 1.0, 0.0);
+    glShadeModel(GL_FLAT);
+
+    vec[0].SetPoint2(100, 300);
+    vec[1].SetPoint2(200, 100);
+    vec[2].SetPoint2(400, 100);
+    vec[3].SetPoint2(500, 300);
 }
+
+
+
 
 void Reshape(int w, int h)
 {
-    glViewport(0, 0, (GLsizei)w, (GLsizei)h);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluOrtho2D(0.0, (GLdouble)w, 0.0, (GLdouble)h);
+        glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        gluOrtho2D(0.0, (GLsizei)w, (GLsizei)h, 0.0);
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
 }
+
+void motion(int x, int y) {
+    if (flag == 8) { // Bspline
+        if (mouseLeftDown) {
+            int min_point = 0;
+            for (int i = 0; i < cnt; i++) {
+                if (distance(vec[min_point].x, vec[min_point].y, x, y) > distance(vec[i].x, vec[i].y, x, y)) min_point = i;
+            }
+            vec[min_point].SetPoint2(x, y);
+        }
+    }
+    glutPostRedisplay();
+}
+
 
 void keyboard(unsigned char key, int x, int y)
 {
@@ -187,9 +245,9 @@ void keyboard(unsigned char key, int x, int y)
         switch (key)
         {
             case 'c':
-                bDrawLine = cohenSutherlandLineClip(rect, x0, y0, x1, y1);
+                bDrawLine = cohenSutherlandLineClip(rect, x2, y2, x3, y3);
 //                bDrawLine = LBLineClip(rect, x0, y0, x1, y1);
-                cout << x0 << " " << y0 << " " << x1 << " " << y1 << endl;
+                cout << x2 << " " << y2 << " " << x3 << " " << y3 << endl;
                 glutPostRedisplay();
                 break;
             case 'a':
@@ -198,12 +256,42 @@ void keyboard(unsigned char key, int x, int y)
                 glutPostRedisplay();
                 break;
             case 'r':
-                restore();
+                x2 = px2; x3 = px3; y2 = py2; y3 = py3;
                 glutPostRedisplay();
                 break;
             default:
                 break;
         }
+    } else if (flag == 2) {
+        
+    } else if (flag == 3) {
+        
+    } else if (flag == 4) {
+        
+    } else if (flag == 5) {
+        
+    } else if (flag == 6) {
+        
+    } else if (flag == 7) {
+        
+    } else if (flag == 8) {
+        switch (key)
+        {
+            case 'c':
+                vec[cnt].x = x;
+                vec[cnt].y = y;
+                cnt++;
+                glutPostRedisplay();
+                break;
+            case 'r':
+                cnt--; // 懒惰删除
+                glutPostRedisplay();
+                break;
+            default:
+                break;
+        }
+    } else if (flag == 9) {
+        
     }
 }
 
@@ -215,21 +303,43 @@ void mouse(int button, int state, int x, int y)
                 {
                     if (bDrawLine)
                     {
-                        x0 = x;
-                        y0 = height - y;
+                        x2 = x;
+                        y2 = y;
                         bDrawLine = false;
                         glutPostRedisplay();
                     }
                     else
                     {
-                        x1 = x;
-                        y1 = height - y;
+                        x3 = x;
+                        y3 = y;
                         bDrawLine = true;
                         glutPostRedisplay();
                     }
                 }
-                px0 = x0; px1 = x1; py0 = y0; py1 = y1; // 存储之前的直线坐标，用于恢复直线
+                px2 = x2; px3 = x3; py2 = y2; py3 = y3; // 存储之前的直线坐标，用于恢复直线
         }
+    } else if (flag == 2) {
+        
+    } else if (flag == 3) {
+        
+    } else if (flag == 4) {
+        
+    } else if (flag == 5) {
+        
+    } else if (flag == 6) {
+        
+    } else if (flag == 7) {
+        
+    } else if (flag == 8) {
+        if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
+            mouseLeftDown = true;
+        }
+
+        if (button == GLUT_LEFT_BUTTON && state == GLUT_UP) {
+            mouseLeftDown = false;
+        }
+    } else if (flag == 9) {
+        
     }
 }
 
@@ -244,46 +354,64 @@ void Display() {
     if (imode == 1) {
         /*动态直线裁剪*/
         glClear(GL_COLOR_BUFFER_BIT);
-        glColor3f(1.0f, 1.0f, 0.0f);
+        glColor3f(0.5f, 0.5f, 1.0f);
         glRectf(rect.xmin, rect.ymin, rect.xmax, rect.ymax);
         if (bDrawLine)
-            LineGL(x0, y0, x1, y1);
+            LineGL(x2, y2, x3, y3);
         glFlush();
-            
         flag = 1;
     } else if (imode == 2) {
         /*绘制贝塞尔曲线*/
 
         flag = 2;
         //Initial();
-    } else if (imode == 2) {
+    } else if (imode == 3) {
         /*动态绘圆*/
         flag = 3;
-    } else if (imode == 2) {
+    } else if (imode == 4) {
         /*动态绘椭圆*/
 
 
         flag = 4;
-    } else if (imode == 2) {
+    } else if (imode == 5) {
         /*动态多边形裁剪*/
 
         flag = 5;
-    } else if (imode == 3) {
+    } else if (imode == 6) {
         /*动态绘制矩形*/
         
         flag = 6;
-    } else if (imode == 5) {
+    } else if (imode == 7) {
         /*二维变换绘制*/
         
         flag = 7;
-    } else if (imode == 6) {
+    } else if (imode == 8) {
         /*二次均匀B样条绘制*/
-    
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glLineWidth(1.5f);
+        glColor3f(1.0, 0.0, 0.0);
+        glBegin(GL_LINE_STRIP);
+        for (int i = 0;i < cnt; i++) {
+            glVertex2f(vec[i].x, vec[i].y);
+        }
+        glEnd();
+
+        glPointSize(10.0f);
+        glColor3f(0.0, 0.0, 1.0);
+        glBegin(GL_POINTS);
+        for (int i = 0; i < cnt; i++) {
+            glVertex2f(vec[i].x, vec[i].y);
+        }
+        glEnd();
+        Bspline(20);
+        glFlush();
+        glutSwapBuffers();
         flag = 8;
-    } else if (imode == 7) {
+    } else if (imode == 9) {
         /*二次均匀B样条绘制*/
         flag = 9;
-    } else if (imode == 9) {
+    } else if (imode == 10) {
         /*动态三角形绘制*/
 
         flag = 10;
@@ -296,8 +424,6 @@ void ProcessMenu(int value) {
     imode = value;
     glutPostRedisplay();
 }
-
-
 
 int main(int argc, char *argv[])
 {
@@ -345,10 +471,12 @@ int main(int argc, char *argv[])
     glMatrixMode(GL_PROJECTION);
     
     init_line();
+    init_Bspline();
     glutDisplayFunc(Display);
-    glutReshapeFunc(Reshape);
     glutKeyboardFunc(keyboard);
     glutMouseFunc(mouse);
+    glutMotionFunc(motion);
+    glutReshapeFunc(Reshape);
 //    glutSpecialFunc(SpecialKeys);
     glutMainLoop();
     return 0;
